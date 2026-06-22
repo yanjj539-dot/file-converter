014be6e7a49e5dd6a3ec044e0ad0fe13ea8c6e57
yan <2114820895@qq.com>
2026-06-22 19:34:52 +0800

    fix: add Green Civic favicon

--- web-projects/green-civic/layouts/default.vue
+++ web-projects/green-civic/layouts/default.vue
@@ -1,3 +1,13 @@
+<script setup lang="ts">
+const assetPath = useAssetPath()
+
+useHead({
+  link: [
+    { rel: 'icon', type: 'image/svg+xml', href: assetPath('/favicon.svg') }
+  ]
+})
+</script>
+
 <template>
   <div class="min-h-screen overflow-x-hidden">
     <div class="site-progress" aria-hidden="true" />
